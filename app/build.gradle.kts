aba79549495b64efe92ed2288972746855bd1a80
mackgaru <dmacevedo00@misena.edu.co>
2023-12-04 12:14:40 -0500

    custom build.gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,6 +105,7 @@ dependencies {
         implementation("androidx.compose.ui:ui-graphics")
         implementation("androidx.compose.ui:ui-tooling-preview")
 
+    //material
         implementation("androidx.compose.material3:material3")
         implementation("androidx.compose.material3:material3-window-size-class")
         implementation("androidx.compose.material:material-icons-extended-android:1.5.4")
